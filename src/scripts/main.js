ca78896da02571d685c94780163cdafd406faa3a
Nikolay Butylkov <kokos.ibo.58@gmail.com>
2021-04-16 10:41:23 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const menu = document.querySelector('.menu__list');
-const toggler = document.querySelector('.menu__img');
-
-toggler.addEventListener('click', () => {
-  menu.classList.toggle('menu__list--active');
-});
